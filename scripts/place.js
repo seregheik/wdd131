f5fd80a5fd1ede5676e147264de6cbd479008eec
Osasere Ikponmwosa <edenosas1@gmail.com>
2026-02-01 00:57:12 +0100

    refactor: remove DOMContentLoaded event listener wrapper from script execution.

--- scripts/place.js
+++ scripts/place.js
@@ -1,38 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
 
-    const yearSpan = document.getElementById('currentyear');
-    if (yearSpan) {
-        yearSpan.textContent = new Date().getFullYear();
+
+const yearSpan = document.getElementById('currentyear');
+if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+}
+
+const lastModified = document.getElementById('lastModified');
+if (lastModified) {
+    lastModified.textContent = `Last Modification: ${document.lastModified}`;
+}
+
+const temperature = 10;
+const windSpeed = 5;
+
+
+function calculateWindChill(temp, speed) {
+    return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
+}
+
+function displayWindChill() {
+    const windChillElement = document.getElementById('wind-chill');
+
+
+    let wc = "N/A";
+
+    if (temperature <= 10 && windSpeed > 4.8) {
+        wc = calculateWindChill(temperature, windSpeed);
+        wc += "°C";
     }
 
-    const lastModified = document.getElementById('lastModified');
-    if (lastModified) {
-        lastModified.textContent = `Last Modification: ${document.lastModified}`;
+    if (windChillElement) {
+        windChillElement.textContent = wc;
     }
+}
 
-    const temperature = 10;
-    const windSpeed = 5;
-
-
-    function calculateWindChill(temp, speed) {
-        return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
-    }
-
-    function displayWindChill() {
-        const windChillElement = document.getElementById('wind-chill');
-
-
-        let wc = "N/A";
-
-        if (temperature <= 10 && windSpeed > 4.8) {
-            wc = calculateWindChill(temperature, windSpeed);
-            wc += "°C";
-        }
-
-        if (windChillElement) {
-            windChillElement.textContent = wc;
-        }
-    }
-
-    displayWindChill();
-});
+displayWindChill();
